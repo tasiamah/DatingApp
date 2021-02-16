4dbb60e5c1128cfeebe7326cedfac8424b1eeb62
tasiamah <t.s.asiamah@student.vu.nl>
2021-02-16 20:16:40 +0100

    implemented routing and installed ngx-toastr

--- front-end/src/app/register/register.component.ts
+++ front-end/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
 import {AccountService} from "../_services/account.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,20 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => console.log(response));
-    this.cancel();
+    this.accountService.register(this.model).subscribe(response => {
+        console.log(response);
+        this.cancel();
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+      });
+
   }
   cancel() {
     this.cancelRegister.emit(false);
