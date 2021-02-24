0b10d590c9f1041a68ed8e9052c71418443ca28d
tasiamah <t.s.asiamah@student.vu.nl>
2021-02-24 13:47:42 +0100

    configured cloudinary and implemented iphoto interface & iphoto service

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -13,7 +13,9 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationService(this IServiceCollection services,
             IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddDbContext<DataContext>(options =>
