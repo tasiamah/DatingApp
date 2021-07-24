3a586366d12b94356419ef7091d574c10dc4dfe6
tasiamah <t.s.asiamah@student.vu.nl>
2021-07-24 18:36:11 +0200

    implemented last active functionality

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Security.Claims;
 
 namespace API.Extensions
 {
     public static class ClaimsPrincipleExtensions
     {
-        public static string GetUsername(this ClaimsPrincipal user)
-        {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        }
+        public static string GetUsername(this ClaimsPrincipal user) =>
+           user.FindFirst(ClaimTypes.Name)?.Value;
+        
+        
+        public static int GetUserId(this ClaimsPrincipal user) => 
+            int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new InvalidOperationException());
+        
     }
 } 
